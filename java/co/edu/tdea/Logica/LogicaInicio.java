5a1d825820de826a41a96844c0c4ac6d2d49396e
JesusDavid658 <jesus.martinez11@correo.tdea.edu.co>
2024-10-30 00:06:20 -0500

    Add files via upload

---
+++ java/co/edu/tdea/Logica/LogicaInicio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.tdea.Logica;
+
+import co.edu.correo.Interfaces.Inicio;
+
+/**
+ *
+ * @author David
+ */
+public class LogicaInicio {
+    public static void main (String[] args) {
+        
+        Inicio panta = new Inicio();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
